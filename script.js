f254fe0691a0bc707036eae29d39417119a22651
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-02-19 16:01:16 -0800

    added beyond function and tidy up js

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-function jediName(firstName, lastName) {
+let jediName = function(firstName, lastName) {
 
-	let fName = firstName.split("").slice(0,2).join("");
-	let lName = lastName.split("").slice(0,3).join("");
+	let fName = firstName.split('').slice(0,2).join('');
+	let lName = lastName.split('').slice(0,3).join('');
 	
 	return lName + fName;
 }
 
-let ans1 = jediName("thara", "wije");
+
+
+let beyond = function(num) {
+
+	if(!isFinite(num)) {
+
+		console.log('And beyond');
+
+	} else if(num > 0) {
+
+		console.log('To infinity');
+
+	} else if(num < 0) {
+
+		console.log('To negative infinity');
+
+	} else if(num === 0){
+
+		console.log('Staying home');
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+let ans1 = jediName('thara', 'wije');
+let ans2 = beyond(0);
