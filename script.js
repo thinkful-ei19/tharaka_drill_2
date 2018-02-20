dbe0a6d83a429d09d472ca8f611c26065346aaa9
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-02-19 16:23:54 -0800

    decode function added

--- script.js
+++ script.js
@@ -32,6 +32,39 @@ let beyond = function(num) {
 
 
 
+let decode = function(word) {
+	
+	let wordList = word.split(' ');
+	let finalWord = '';
+
+	for(let i = 0; i < wordList.length; i++) {
+		let firstLetter = wordList[i][0].toLowerCase();
+
+		if(firstLetter === 'a') {
+
+			finalWord += wordList[i][1];
+
+		} else if(firstLetter === 'b') {
+
+			finalWord += wordList[i][2];
+
+		} else if(firstLetter === 'c') {
+
+			finalWord += wordList[i][3];
+
+		} else if(firstLetter === 'd') {
+
+			finalWord += wordList[i][4];
+
+		} 
+
+	}
+	
+	return finalWord;
+
+}
+
+
 
 
 
@@ -41,3 +74,4 @@ let beyond = function(num) {
 
 let ans1 = jediName('thara', 'wije');
 let ans2 = beyond(0);
+let ans3 = decode('craft block argon meter bells brown croon droop');
